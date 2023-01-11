16530876eac77f2230f676fcb336db7348c235be
Jonny Hall <car87maniac@googlemail.com>
2023-01-11 10:35:30 +0000

    Save for close

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
 import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="container">
-      <Header headerLabel="This is the header" />
-    </div>
-  );
-}
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="hello" />
     </header>
   );
 };
@@ -16,4 +18,6 @@ Header.propTypes = {
   headerLabel: PropTypes.string.isRequired,
 };
 
+// const headingStyle = {
+// { color: "red", backgroundColor: "black" }}
 export default Header;
